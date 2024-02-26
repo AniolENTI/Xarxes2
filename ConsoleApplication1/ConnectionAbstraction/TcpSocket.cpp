39cf55fbaa31e44ae108b8099f96d0aa5ccf75e6
Aniol <aniol.morato@enti.cat>
2024-02-26 10:54:52 +0100

    Xat dia 4
    Connection abstraction avançat (ns si acabat)

--- ConsoleApplication1/ConnectionAbstraction/TcpSocket.cpp
+++ ConsoleApplication1/ConnectionAbstraction/TcpSocket.cpp
@@ -1,1 +1,94 @@
 #include "TcpSocket.h"
+
+bool TcpSocket::Connect(std::string ip, unsigned short port)
+{
+	return connect(ip, port) == sf::Socket::Done;
+}
+
+void TcpSocket::Receive()
+{
+	Packet packet;
+	sf::Socket::Status status = receive(packet);
+
+	switch (status)
+	{
+	case sf::Socket::Done:
+		ProcessPacket(packet);
+		break;
+	case sf::Socket::NotReady:
+		break;
+	case sf::Socket::Partial:
+		break;
+	case sf::Socket::Disconnected:
+		_onSocketDisconnectMutex.lock();
+		for (OnSocketDisconnect onDisconnect : _onSocketDisconnectList)
+		{
+			onDisconnect(this);
+		}
+		_onSocketDisconnectList.clear();
+		_onSocketDisconnectMutex.unlock();
+		break;
+	case sf::Socket::Error:
+		break;
+	default:
+		break;
+	}
+}
+
+bool TcpSocket::Send(Packet::PacketKey key)
+{
+	Packet signedPacket;
+	signedPacket << key;
+
+	return send(signedPacket) == sf::Socket::Done;
+}
+
+bool TcpSocket::Send(Packet::PacketKey key, Packet packet)
+{
+	Packet signedPacket;
+	signedPacket << key;
+
+	signedPacket.append(packet.getData(), packet.getDataSize());
+
+	return send(signedPacket) == sf::Socket::Done;
+}
+
+void TcpSocket::Subscribe(Packet::PacketKey key, OnReceivePacket onReceivePacket)
+{
+	_subscriptionsMutex.lock();
+	_subscription[key] = onReceivePacket;
+	_subscriptionsMutex.unlock();
+}
+
+void TcpSocket::SubscribeAsync(Packet::PacketKey key, OnReceivePacket onReceivePacket)
+{
+	std::thread* subscribeThread = new std::thread(&TcpSocket::Subscribe, this, key, onReceivePacket);
+	subscribeThread->detach();
+}
+
+void TcpSocket::SubscribeOnDisconnect(OnSocketDisconnect onSocketDisconnect)
+{
+	_onSocketDisconnectMutex.lock();
+
+	_onSocketDisconnectList.push_back(onSocketDisconnect);
+
+	_onSocketDisconnectMutex.unlock();
+}
+
+void TcpSocket::ProcessPacket(Packet packet)
+{
+	Packet::PacketKey key;
+	packet >> key;
+
+	std::map<Packet::PacketKey, OnReceivePacket>::iterator it;
+
+	_subscriptionsMutex.lock();
+
+	it = _subscription.find(key);
+	if (it != _subscription.end())
+	{
+		it->second(packet);
+	}
+
+	_subscriptionsMutex.unlock();
+}
